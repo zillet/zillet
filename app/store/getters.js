97a7286747be00aac484af2ac0aae9bb04d4c311
man15h <manish9461@gmail.com>
2019-05-23 15:00:07 +0530

    bech32 address spec

--- app/store/getters.js
+++ app/store/getters.js
@@ -1,6 +1,13 @@
 import * as util from '@zilliqa-js/util';
+import { toBech32Address } from '@zilliqa-js/crypto';
 const { units, BN } = util;
-export const Account = state => state.wallet;
+export const Account = state => {
+  let wallet = state.wallet;
+  if (state.wallet.address) {
+    wallet.bech32Address = toBech32Address(state.wallet.address);
+  }
+  return wallet;
+};
 export const LoggedIn = state => !!state.wallet.privateKey;
 export const Online = () => window.navigator.onLine;
 export const Prices = state => state.prices;
