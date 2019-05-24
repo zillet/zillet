4689b639e9b9873d11ec65c741e9be4ef4d8b074
man15h <manish9461@gmail.com>
2019-05-24 11:42:41 +0530

    Blog link added

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="container">
     <Notifications />
+    <div
+      v-if="showNoti"
+      class="bg-teal-600 rounded w-full p-2 text-white
+      flex items-center justify-between mt-4">
+      <div class="px-6">
+        Addresses are now defaulted to the new Zilliqa bech32 standard, your funds are not affected in any way.
+        <a
+          href="https://blog.zilliqa.com/zilliqa-migrates-to-new-address-format-bf1fa6d7e41d"
+          target="_blank"
+          class="underline italic"
+          rel="nofollow">
+          Read full announcement here
+        </a>
+      </div>
+      <i
+        class="eva eva-close-circle-outline text-xl pr-2 cursor-pointer"
+        @click="hideNoti" />
+    </div>
     <Navigation />
     <div class="w-full">
       <div class="card">
@@ -22,6 +40,25 @@ export default {
     Navigation,
     Footer,
     Onboarding
+  },
+  data() {
+    return {
+      showNoti: false
+    };
+  },
+  beforeMount() {
+    const noti = localStorage.getItem('hide_noti');
+    if (noti && noti == 'true') {
+      this.showNoti = false;
+    } else {
+      this.showNoti = true;
+    }
+  },
+  methods: {
+    hideNoti() {
+      localStorage.setItem('hide_noti', true);
+      this.showNoti = false;
+    }
   }
 };
 </script>
