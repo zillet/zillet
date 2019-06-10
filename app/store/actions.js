638ee9506c5b957e73a505a50b18f3075d71c0f7
man15h <manish9461@gmail.com>
2019-06-10 13:47:31 +0530

    remove 0x from address

--- app/store/actions.js
+++ app/store/actions.js
@@ -55,7 +55,7 @@ export function getTransactions({ commit }, data) {
     var type = data.type || 'all';
     commit('LOADING');
     this.$axios
-      .$get(`${config.VIEWBLOCK_URL}0x${address}`, {
+      .$get(`${config.VIEWBLOCK_URL}${address}`, {
         params: {
           network: network,
           page: page,
